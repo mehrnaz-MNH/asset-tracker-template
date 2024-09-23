cf455f9c283bbec1ad4fc552089bc3c70e7b1f84
mehrnaz-MNH <mnh.mehrnaz@gmail.com>
2024-09-24 09:22:09 +1000

    read the app - got my api key , will try to render the assets first

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,29 +5,11 @@ import Link from "next/link";
 const Home: NextPage = () => (
   <Flex justifyContent="center" alignItems="center" height="100vh" flexDirection="column">
     <Heading>🚀 Asset Tracker</Heading>
-    <Text>This is the repository template for the Asset Tracker assessment.</Text>
-    <Text>The app is setup with the following:</Text>
-    <UnorderedList pl="10px">
-      <ListItem>
-        <Link href="https://nextjs.org/docs" target="_blank">
-          Next.js
-        </Link>
-      </ListItem>
-      <ListItem>
-        <Link href="https://chakra-ui.com/docs/getting-started" target="_blank">
-          Chakra UI
-        </Link>
-      </ListItem>
-      <ListItem>
-        <Link
-          href="https://tanstack.com/query/latest/docs/framework/react/overview"
-          target="_blank"
-        >
-          React Query
-        </Link>
-      </ListItem>
-    </UnorderedList>
   </Flex>
 );
 
 export default Home;
+
+// https://tanstack.com/query/latest/docs/framework/react/overview
+// https://chakra-ui.com/docs/getting-started
+// https://nextjs.org/docs
